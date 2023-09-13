60726dc6d1ab40e98579b6862bdafb0b30ef0abb
isama78 <isa21005@byui.edu>
2023-09-13 20:10:32 -0300

    first commit in WDD230

---
+++ scripts/getdates.js
@@ -0,0 +1,20 @@
+const getDate = () => {
+    let year = new Date().getFullYear()
+    return year
+}
+
+const visitsCounter = () => {
+    let visits = parseInt(localStorage.getItem('visits'))
+    if(isNaN(visits)) {
+        visits = 1
+    } else {
+        visits += 1
+    }
+    
+    localStorage.setItem('visits', visits)
+    return visits
+}
+
+document.querySelector('#year').innerHTML += getDate()
+document.querySelector('#lastModified').innerHTML = `Last modification: ${document.lastModified}` 
+document.querySelector('#visits').innerHTML = `Page Visits ${visitsCounter()}` 
