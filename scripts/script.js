3cfffd486307dc2fe14f03f41f79516fd54df4e3
isama78 <isa21005@byui.edu>
2023-11-02 22:18:20 -0300

    form group activity completed

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,34 @@ mainMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('show')
     navBar.classList.toggle('show')
 })
+
+//Handle input range
+if (document.querySelector("#pi_input")) {
+    const value = document.querySelector("#value");
+    const input = document.querySelector("#pi_input");
+    value.textContent = input.value;
+    input.addEventListener("input", (event) => {
+        value.textContent = event.target.value;
+    });
+}
+
+
+
+if (document.querySelector('#password')) {
+    const pass1 = document.querySelector('#password')
+    const pass2 = document.querySelector('#password2')
+    const message = document.querySelector('#message')
+    const checkSame = () => {
+        if (pass1.value !== pass2.value) {
+            message.style.display = "block";
+            message.textContent = "❗Key Phrases DO NOT MATCH!"
+            pass2.value = ''
+            pass2.focus()
+        } else {
+            message.style.display = "none";
+        }
+    }
+    pass2.addEventListener('focusout', () => {
+        checkSame()
+    })
+}
