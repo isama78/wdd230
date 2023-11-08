2464ee6e68f0d33b67705e8ae758302d6b1ae7b9
isama78 <isa21005@byui.edu>
2023-11-07 23:21:13 -0300

    Directory page in process

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,5 +1,6 @@
 
 
+
 const getDate = () => {
     let year = new Date().getFullYear()
     return year
@@ -7,12 +8,12 @@ const getDate = () => {
 
 const visitsCounter = () => {
     let visits = parseInt(localStorage.getItem('visits'))
-    if(isNaN(visits)) {
+    if (isNaN(visits)) {
         visits = 1
     } else {
         visits += 1
     }
-    
+
     localStorage.setItem('visits', visits)
     return visits
 }
@@ -21,15 +22,15 @@ const calculatorBetweenVisits = () => {
     let rightNow = new Date()
     let lastVisit = window.localStorage.getItem('lastVisit')
     localStorage.setItem('lastVisit', rightNow)
-    let diff = new Date(rightNow)  - new Date(lastVisit) 
+    let diff = new Date(rightNow) - new Date(lastVisit)
     let diffInDays = diff / (1000 * 60 * 60 * 24)
     return lastVisit === null ? 'Welcome! Let us know if you have any questions.' : diffInDays < 1 ? 'Back so soon! Awesome!' : `You last visited ${diffInDays.toFixed(0)} days ago.`
 }
 
-if(document.querySelector('#welcome')) document.querySelector('#welcome').innerHTML = calculatorBetweenVisits()
+if (document.querySelector('#welcome')) document.querySelector('#welcome').innerHTML = calculatorBetweenVisits()
 
 document.querySelector('#year').innerHTML += getDate()
-document.querySelector('#lastModified').innerHTML = `Last modification: ${document.lastModified}` 
+document.querySelector('#lastModified').innerHTML = `Last modification: ${document.lastModified}`
 document.querySelector('#visits') ? document.querySelector('#visits').innerHTML = `Page Visits ${visitsCounter()}` : null
 const darkMode = document.querySelector('.slider')
 darkMode.addEventListener('click', () => {
@@ -37,7 +38,7 @@ darkMode.addEventListener('click', () => {
 })
 
 const timeAndDate = new Date()
-if(document.querySelector('#timestamp')) document.querySelector('#timestamp').value = timeAndDate
+if (document.querySelector('#timestamp')) document.querySelector('#timestamp').value = timeAndDate
 
 let menu = document.querySelector('#menu')
 let navBar = document.querySelector('#nav-bar')
