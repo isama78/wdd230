5dcdf2d0eb0c4327ae9f92871bf0171c4d114489
isama78 <isa21005@byui.edu>
2023-11-09 20:45:05 -0300

    group activity in progress

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,28 @@
 const baseURL = 'https://isama78.github.io/wdd230/'
-const linksURL = 'data/links.json'
+const linksURL = 'http://127.0.0.1:5500/data/links.json'
+let ulLinks = document.querySelector('.weeks')
+
+const getLinks = async url => {
+    const resp = await fetch(url)
+    const data = await resp.json()
+    displayLinks(data.lessons)
+}
+
+const displayLinks = weeks => {
+    weeks.forEach(week => {
+        let li = document.createElement('li')
+        li.textContent = `Week ${week.lesson}: ` 
+        let a = document.createElement('a')
+        week.links.forEach(link => {
+            a.setAttribute('href', `${baseURL}${link.url}`)
+            a.innerHTML += `${link.title} |` 
+        })
+        li.append(a)
+        ulLinks.append(li)
+    });
+}
+
+
+
+
+getLinks(linksURL)
